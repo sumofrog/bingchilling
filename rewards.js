f90ea838165b3a265b9e5855db8c9923cbf78737
Vince <61852029+sumofrog@users.noreply.github.com>
2023-02-25 18:23:06 -0500

    Add files via upload

---
+++ rewards.js
@@ -0,0 +1,48 @@
+    //last row is for edge search
+    const searchTerms = [
+        "hello", "buddy", "my head", "dbz",
+        "crest", "fallen", "antsy", "blue",
+        "lamp", "holdup", "black eyed", "shade",
+        "shoes", "man time", "juicy", "couture",
+        "blessed", "blursed", "boomer", "backend", 
+        "pumpkin", "moose", "pokemon", "temper" 
+    ]; 
+
+    const bing = "https://bing.com/search?q=";
+
+    function isEdge() {
+        try {
+            for (brand_version_pair of navigator.userAgentData.brands) {
+                if (brand_version_pair.brand == "Microsoft Edge"){
+                    return true;
+                }
+            }
+        } catch (err) {
+            return false;
+        }
+        return false;
+    }
+
+    function timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    async function slowedCode() {
+        await this.timeout(Math.random() * 2000 + 500) // Wait random amount of time between [0.5, 2.5] seconds
+    }
+
+    async function slowedForLoop() {
+        if (isEdge()) {
+            for (i = 20; i < searchTerms.length; i++) {
+                window.open(bing + searchTerms[i]);
+                await this.timeout(Math.random() * 3000 + 500)                    
+            }      
+        } else {
+            for (i = 0; i < 20; i++) {
+                window.open(bing + searchTerms[i]);
+                await this.timeout(Math.random() * 3000 + 500)
+            }
+        }
+    }
+
+    slowedForLoop();
